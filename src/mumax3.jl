b401c099238db03d7ac0d948bb3bf6ac0a7b8069
Dreycen Foiles <foilesdreycen@gmail.com>
2022-11-30 13:23:01 -0600

    first commit

---
+++ src/mumax3.jl
@@ -0,0 +1,41 @@
+module mumax3
+
+using NPZ
+using Glob
+using CSV 
+using DataFrames
+
+export run_mumax3
+
+function run_mumax3(script)
+    # Run mumax3
+
+    name = "tmp"
+    scriptname = name * ".mx3"
+    outputdir = name * ".out"
+
+    write(scriptname, script)
+
+    # The > nul is to suppress the output
+    run(`mumax3 -f $scriptname`)
+    run(`mumax3-convert -numpy $outputdir/"*.ovf"`) # Convert OVF to NPY 
+
+    data_array = [] 
+
+    # Read in all the NPY files and store them in a single array
+    for file in glob(outputdir * "/m*.npy")
+        push!(data_array, npzread(file))
+    end
+
+    fields = cat(data_array..., dims=5)
+
+    table = CSV.File(outputdir * "/table.txt") |> DataFrame
+
+    rm(outputdir, recursive=true) # Remove output directory
+    rm(scriptname) # Remove script file
+
+    return fields, table
+
+end
+    
+end # module mumax3
---
+++ test/Std4.jl
@@ -0,0 +1,31 @@
+
+using mumax3 
+using NPZ
+using Glob 
+using BenchmarkTools
+using CSV 
+
+function std4()
+    """
+    SetGridsize(128, 32, 1)
+    SetCellsize(500e-9/128, 125e-9/32, 3e-9)
+
+    Msat  = 800e3
+    Aex   = 13e-12
+    alpha = 0.02
+
+    m = uniform(1, .1, 0)
+    relax()
+    save(m)    // relaxed state
+
+    autosave(m, 200e-12)
+    tableautosave(10e-12)
+
+    B_ext = vector(-24.6E-3, 4.3E-3, 0)
+    run(1e-9)
+    """
+end
+
+fields, table = run_mumax3(std4()) 
+
+CSV.write("std4.csv", table)
